ec20eea3ecc3089fd8f24514f811a33bf746c9e4
small_sunshine <jioo0224@naver.com>
2018-01-19 17:14:40 +0900

    소스 수정하여 자동으로 뜻 번호 반영

--- main.js
+++ main.js
@@ -1,40 +1,62 @@
 const config = require('./config.json')
 const pg = require('pg-promise')()
+const async = require('async')
 const words = require('./words.json');
 
 (async () => {
   try {
     let db = pg(config.sql)
     db = await db.connect()
-    words.forEach(async (value, index, thisarr) => {
-      await inputWord(db, value)
+    async.eachLimit(words, 1, (value, callback) => {
+      inputWord(db, value, callback)
+    }, (err) => {
+      if (err) {
+        console.error(err)
+        process.exit()
+      } else process.exit()
     })
   } catch (e) {
     console.error(e)
   }
 })()
 
-async function inputWord (db, word) {
-  console.log('new word data: ')
-  console.log(word)
-  let i = await db.any('SELECT * FROM public.kkutu_ko WHERE _id = $1', word.id)
-  console.log('default word data: ')
-  console.log(i[0])
+async function inputWord (db, word, callback) {
   try {
+    console.log('new word data: ')
+    console.log(word)
+    let i = await db.any('SELECT * FROM public.kkutu_ko WHERE _id = $1', word.id)
+    console.log('default word data: ')
+    console.log(i[0])
+
     if (!i[0]) {
+      let thisOffset = 0
+      word.mean = word.mean.replace(/(\/mean)/g, (match, p1, offset, string) => {
+        thisOffset++
+        return '＂' + thisOffset + '＂'
+      })
       await db.any('INSERT INTO public.kkutu_ko (_id, type, mean, flag, theme) VALUES ($1, $2, $3, $4, $5)', [
         word.id, word.type, word.mean, word.flag, word.theme
       ])
+      /* console.log('INSERT INTO public.kkutu_ko (_id, type, mean, flag, theme) VALUES (' + word.id + ', ' +
+      word.type + ', ' + word.mean + ', ' + word.flag + ', ' + word.theme + ')') // test data */
     } else {
-      if (i[0].type !== word.type &&
-      i[0].mean !== word.mean &&
-      i[0].theme !== word.theme) {
+      let themeOffset = (i[0].theme.match((/,/g)) || []).length
+      let thisOffset = 1
+      word.mean = word.mean.replace(/(\/mean)/g, (match, p1, offset, string) => {
+        thisOffset++
+        return '＂' + (themeOffset + thisOffset) + '＂'
+      })
+      console.log(i[0].mean !== word.mean)
+      if (i[0].type !== word.type || i[0].mean !== word.mean || i[0].theme !== word.theme) {
         await db.any('UPDATE public.kkutu_ko SET type=$1, mean=$2, theme=$3 WHERE _id = $4', [
           i[0].type + ',' + word.type,
           i[0].mean + '  ' + word.mean,
           i[0].theme + ',' + word.theme,
           word.id
         ])
+        /* console.log('UPDATE public.kkutu_ko SET type=' + i[0].type + ',' + word.type +
+          ', mean=' + i[0].mean + '  ' + word.mean + ', theme=' + i[0].theme + ',' + word.theme +
+          ' WHERE _id = ' + word.id) // test code */
       } else {
         console.log('same word. not update.')
       }
@@ -43,8 +65,8 @@ async function inputWord (db, word) {
     i = await db.any('SELECT * FROM public.kkutu_ko WHERE _id = $1', word.id)
     console.log('inputed data: ')
     console.log(i[0])
+    callback()
   } catch (e) {
-    console.log('error')
-    console.error(e)
+    callback(e)
   }
 }
